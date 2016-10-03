a66682b1b4f8859903af7a439fe5a388d4f809e1
Josh Leidl <joshleidl@gmail.com>
2016-10-03 12:12:28 -0600

    Added useList file to use the linked list files

--- linkedList.java
+++ linkedList.java
@@ -98,8 +98,4 @@ public class linkedList
 			}
 		}
 	}
-	
-	
-	//public addElement()
-
 }
---
+++ useList.java
@@ -0,0 +1,21 @@
+import linkedList;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class useList
+{
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		
+		String input = " ";
+		
+		linkedList mainList = new linkedList();
+		
+		while(input != "5")
+		{
+			
+		}
+	}
+}
