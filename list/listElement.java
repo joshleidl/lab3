d647cc9b1b4e95322b2c8b19189cd4df422b5326
joshleidl <joshleidl@gmail.com>
2016-10-24 11:05:16 -0600

    Updated

--- list/listElement.java
+++ list/listElement.java
@@ -4,27 +4,23 @@ public class listElement
 {
 	private int data;
 	private listElement next;
-	private listElement prev;
 	
 	public listElement()
 	{
 		this.data = 0;
 		this.next = null;
-		this.prev = null;
 	}
 	
 	public void copy(listElement copyFrom)
 	{
 		this.data = copyFrom.getData();
 		this.next = copyFrom.getNext();
-		this.prev = copyFrom.getPrev();
 	}
 	
 	public listElement(int d)
 	{
 		this.data = d;
 		this.next = null;
-		this.prev = null;
 	}
 	
 	public int getData()
@@ -42,14 +38,5 @@ public class listElement
 		return this.next;
 	}
 	
-	public void setPrev(listElement le)
-	{
-		this.prev = le;
-	}
-	
-	public listElement getPrev()
-	{
-		return this.prev;
-	}
 }
 
